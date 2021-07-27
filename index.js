f0ee7f8cb6a1d56dc3982250c204582ce18ba6ef
zNotChill <59340653+zNotChill@users.noreply.github.com>
2021-07-27 22:35:22 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+document.getElementById('guardian').addEventListener('mouseenter', () => {
+    document.getElementById('guardian').innerHTML = "Guardian#1848"
+})
+
+document.getElementById('guardian').addEventListener('mouseleave', () => {
+    document.getElementById('guardian').innerHTML = "Telemachos57"
+})
+
+document.getElementById('iduckz').addEventListener('mouseenter', () => {
+    document.getElementById('iduckz').innerHTML = "Duck King#0881"
+})
+
+document.getElementById('iduckz').addEventListener('mouseleave', () => {
+    document.getElementById('iduckz').innerHTML = "IDuckz_"
+})
+
+document.getElementById('znotchill').addEventListener('mouseenter', () => {
+    document.getElementById('znotchill').innerHTML = "chxll#8438"
+})
+
+document.getElementById('znotchill').addEventListener('mouseleave', () => {
+    document.getElementById('znotchill').innerHTML = "zNotChill"
+})
